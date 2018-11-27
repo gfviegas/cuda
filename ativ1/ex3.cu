a75f51de2507c7ad3f8af3ab46a1f57c51454b08
Gustavo Viegas <webviegas@gmail.com>
2018-11-27 20:41:59 -0200

    Exercicio 4

--- ativ1/ex3.cu
+++ ativ1/ex3.cu
@@ -1,20 +1,18 @@
-#include <stdlib.h>
 #include <stdio.h>
 
 __global__ void helloFromGPU() {
-  printf("Hello World from GPU! %d %d\n", threadIdx.x, blockIdx.x);
+  if (threadIdx.x < 20 && blockIdx.x < 20)
+    printf("Hello World from GPU! %d %d\n", threadIdx.x, blockIdx.x);
 }
 
 int main(int argc, char**argv) {
   printf("Hello World from CPU!\n");
 
-  long long int threads = 2 * 1024 * 1024;
-  long long int blocks = 1024 * 1024;
-  helloFromGPU<<<threads, blocks>>>();
+  // Não tá funcionando colocar esse numero bem grande...
+  long long int blocks = 1024;
+  long long int threads = 2;
+  helloFromGPU<<<blocks, threads>>>();
 
   cudaDeviceReset();
-  // cudaDeviceSynchronize();
-  // CHECK(cudaDeviceReset());
-
   return 0;
 }
---
+++ ativ1/ex4.cu
@@ -0,0 +1,33 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <math.h>
+
+__global__ void fibonacci(int n) {
+  int novoN = abs((n - (int) blockIdx.x + (int) threadIdx.x) % n);
+
+  int aux = novoN;
+  long long int a = 0;
+  long long int b = 1;
+
+  while (aux-- > 1) {
+    long long int t = a;
+    a = b;
+    b += t;
+  }
+
+  printf("Fibonacci de %d = %lld\n", novoN, b);
+}
+
+int main(int argc, char**argv) {
+  printf("Hello World from CPU!\n");
+
+  long long int threads = 1024;
+  long long int blocks = 1024;
+  fibonacci<<<threads, blocks>>>(60);
+
+  cudaDeviceSynchronize();
+  // cudaDeviceReset();
+  // CHECK(cudaDeviceReset());
+
+  return 0;
+}
